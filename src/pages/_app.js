e0fb96513f43e1b222cf05143d9c4e88b19a303b
rushita-vachhani <heli.kapadia01@gmail.com>
2024-05-25 15:47:50 +0400

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import "@/styles/globals.scss";
 import "@/styles/reset.css";
 import Script from "next/script";
+import { ScrollControlProvider } from "@/context/ScrollControlContext";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
-      <Script src='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' />
+      <ScrollControlProvider>
+        <Component {...pageProps} />
+        <Script src="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </ScrollControlProvider>
     </>
   );
 }
