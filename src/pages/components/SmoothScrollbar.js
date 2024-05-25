f07c9213bfb8d208802ad095f4a0f30f4d7f3a46
rushita-vachhani <heli.kapadia01@gmail.com>
2024-05-25 15:42:38 +0400

    fix the error

--- src/pages/components/SmoothScrollbar.js
+++ src/pages/components/SmoothScrollbar.js
@@ -2,7 +2,6 @@ import { useScrollControl } from "../../../context/ScrollControlContext";
 import React, { useRef, useEffect } from "react";
 import Scrollbar from "smooth-scrollbar";
 
-
 const SmoothScrollbar = ({ children }) => {
   const scrollRef = useRef(null);
   const { isScrollEnabled } = useScrollControl();
@@ -21,13 +20,7 @@ const SmoothScrollbar = ({ children }) => {
   return (
     <div
       ref={scrollRef}
-      style={{
-        width: "100%",
-        height: "100vh",
-        overflow: "hidden",
-        margin: "0",
-        padding: "0",
-      }}
+      style={{ width: "100%", height: "100vh", overflow: "hidden", margin: "0", padding: "0" }}
     >
       {children}
     </div>
--- src/pages/components/customNavbar.js
+++ src/pages/components/customNavbar.js
@@ -2,52 +2,52 @@ import { useState, useEffect } from "react";
 import styles from "@/styles/comps/navbar.module.scss";
 import Image from "next/image";
 import logo from "../../../public/assets/png/logo.png";
-import { useScrollControl } from "../../../context/ScrollControlContext"; // Adjust the import path accordingly
+import { useScrollControl } from "../../../context/ScrollControlContext"; 
 
 const CustomNavbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const { disableScroll, enableScroll } = useScrollControl();
-
-  const handleMenuToggle = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  useEffect(() => {
-    if (menuOpen) {
-      disableScroll();
-    } else {
-      enableScroll();
-    }
-  }, [menuOpen, disableScroll, enableScroll]);
-
-  return (
-    <div className={styles.navbar}>
-      <div className={styles.content}>
-        <Image src={logo} alt='Logo' width={200} height={100} />
-        <div className={styles["menu-toggle"]} onClick={handleMenuToggle}>
-          <div></div>
-          <div></div>
-          <div></div>
+    const [menuOpen, setMenuOpen] = useState(false);
+    const { disableScroll, enableScroll } = useScrollControl();
+  
+    const handleMenuToggle = () => {
+      setMenuOpen(!menuOpen);
+    };
+  
+    useEffect(() => {
+      if (menuOpen) {
+        disableScroll();
+      } else {
+        enableScroll();
+      }
+    }, [menuOpen, disableScroll, enableScroll]);
+  
+    return (
+      <div className={styles.navbar}>
+        <div className={styles.content}>
+          <Image src={logo} alt="Logo" width={200} height={100} />
+          <div className={styles["menu-toggle"]} onClick={handleMenuToggle}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <ul>
+            <li>Category</li>
+            <li>About us</li>
+            <li>Service</li>
+            <li>Blog</li>
+            <li>Contact us</li>
+          </ul>
         </div>
-        <ul className={styles.menu}>
-          <li>Category</li>
-          <li>About us</li>
-          <li>Service</li>
-          <li>Blog</li>
-          <li>Contact us</li>
-        </ul>
+        <div className={`${styles.sidebar} ${menuOpen ? styles.active : ""}`}>
+          <ul>
+            <li onClick={handleMenuToggle}>Category</li>
+            <li onClick={handleMenuToggle}>About us</li>
+            <li onClick={handleMenuToggle}>Service</li>
+            <li onClick={handleMenuToggle}>Blog</li>
+            <li onClick={handleMenuToggle}>Contact us</li>
+          </ul>
+        </div>
       </div>
-      <div className={`${styles.sidebar} ${menuOpen ? styles.active : ""}`}>
-        <ul>
-          <li onClick={handleMenuToggle}>Category</li>
-          <li onClick={handleMenuToggle}>About us</li>
-          <li onClick={handleMenuToggle}>Service</li>
-          <li onClick={handleMenuToggle}>Blog</li>
-          <li onClick={handleMenuToggle}>Contact us</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-export default CustomNavbar;
+    );
+  };
+  
+  export default CustomNavbar;
